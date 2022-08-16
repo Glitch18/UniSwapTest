9bd99119246ec905a47eeeaa08d0831a249d1c47
Glitch18 <atharvakn@gmail.com>
2022-08-16 20:38:25 +0530

    .gitignore is now working

--- test/BitMathTest.ts
+++ test/BitMathTest.ts
@@ -5,18 +5,23 @@ import { loadFixture } from '@nomicfoundation/hardhat-network-helpers';
 
 describe('BitMath', () => {
   let bitMath: BitMathTest__WC__BitMathTest_compiled
-  const fixture = async () => {
-    const factory = await getStarknetContractFactory('BitMathTest')
-    const contract = await factory.deploy()
-    return (await contract.deployed()) as BitMathTest__WC__BitMathTest_compiled
-  }
-  beforeEach('deploy BitMathTest', async () => {
-    bitMath = await loadFixture(fixture);
-  })
+  // const fixture = async () => {
+  //   const factory = await getStarknetContractFactory('BitMathTest')
+  //   const contract = await factory.deploy()
+  //   return (await contract.deployed()) as BitMathTest__WC__BitMathTest_compiled
+  // }
+  // beforeEach('deploy BitMathTest', async () => {
+  //   bitMath = await loadFixture(fixture);
+  // })
 
   describe('#mostSignificantBit', () => {
     it('0', async () => {
-      await expect(bitMath.mostSignificantBit_e6bcbc65({low:0, high:0})).to.be.reverted
+      // await expect(bitMath.mostSignificantBit_e6bcbc65({low:0, high:0})).to.be.reverted
+      const factory = getStarknetContractFactory('BitMathTest')
+      const contract = await (factory.deploy()) as BitMathTest__WC__BitMathTest_compiled
+      console.log(`Deploying on ${contract.address}`)
+      await contract.deployed()
+      console.log('Deployed')
     })
   })
 })

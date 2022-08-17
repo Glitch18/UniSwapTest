dc60de8d2d4cdcd5a92b5ccf175485c894abcaac
Glitch18 <atharvakn@gmail.com>
2022-08-17 18:36:39 +0530

    Fix wrong function call

--- test/BitMathTest.ts
+++ test/BitMathTest.ts
@@ -14,29 +14,29 @@ describe('BitMath', () => {
 
   describe('#mostSignificantBit', () => {
     it('0', async () => {
-      await expect(bitMath.leastSignificantBit_d230d23f({low:0, high:0})).to.be.rejected
+      await expect(bitMath.mostSignificantBit_e6bcbc65({low:0, high:0})).to.be.rejected
     })
     it('1', async () => {
-      const res = await bitMath.leastSignificantBit_d230d23f({low: 1, high: 0})
+      const res = await bitMath.mostSignificantBit_e6bcbc65({low: 1, high: 0})
       expect(res[0]).to.eq(0)
     })
     it('2', async () => {
-      const res = await bitMath.leastSignificantBit_d230d23f({low: 2, high: 0})
+      const res = await bitMath.mostSignificantBit_e6bcbc65({low: 2, high: 0})
       expect(res[0]).to.eq(1)
     })
     it('all powers of 2', async () => {
       const low = [...Array(128)].map(
-        (_, i) => bitMath.leastSignificantBit_d230d23f({low: new BN(2).pow(new BN(i)), high: 0})
+        (_, i) => bitMath.mostSignificantBit_e6bcbc65({low: new BN(2).pow(new BN(i)), high: 0})
       )
       const high = [...Array(128)].map(
-        (_, i) => bitMath.leastSignificantBit_d230d23f({low: 0, high: new BN(2).pow(new BN(i))})
+        (_, i) => bitMath.mostSignificantBit_e6bcbc65({low: 0, high: new BN(2).pow(new BN(i))})
       )
       const results = await Promise.all([...low, ...high])
       const res = results.map((element) => element[0].toNumber());
       expect(res).to.deep.eq([...Array(256)].map((_, i) => i));
     })
     it('uint256(-1)', async () => {
-      const res = await bitMath.leastSignificantBit_d230d23f({
+      const res = await bitMath.mostSignificantBit_e6bcbc65({
         low: new BN(2).pow(new BN(128)).sub(new BN(1)), 
         high: new BN(2).pow(new BN(128)).sub(new BN(1)),
       })
